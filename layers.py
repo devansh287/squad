b09c1ad2e445dedf3f69c2e53d9c900390d526a7
devansh287 <devansh@stanford.edu>
2021-03-13 16:25:27 +0530

    Update layers.py

--- layers.py
+++ layers.py
@@ -336,21 +336,21 @@ class QAEncoder(nn.Module):
         self.num_heads = 10 # Likewise, this was suggested by the QANet paper
         self.drop_prob = drop_prob
         # Layer Norms - N.B. designed to handle input size different to hidden size
-        self.init_layer_norm = nn.LayerNorm(input_size)
-        self.layer_norm = nn.LayerNorm(hidden_size)
+        self.init_layer_norm = nn.LayerNorm(input_size).to(device)
+        self.layer_norm = nn.LayerNorm(hidden_size).to(device)
         # Convolutions - N.B. designed to handle input size different to hidden size
         self.init_conv = nn.Conv1d(in_channels=input_size,
                                    out_channels=hidden_size,
                                    kernel_size=self.kernel_size,
                                    padding=3,
-                                   groups=hidden_size)
+                                   groups=hidden_size).to(device)
         self.convs = []
         for i in range(num_layers-1):
             self.convs.append(nn.Conv1d(in_channels=hidden_size,
                                         out_channels=hidden_size,
                                         kernel_size=self.kernel_size,
                                         padding=3,
-                                        groups=hidden_size))
+                                        groups=hidden_size).to(device))
         # Multi-Head Self Attention
         self.att = MultiHeadSelfAttention(hidden_size, self.num_heads, drop_prob=drop_prob)
         self.pos_encoder = PositionalEncoding(input_size, dropout=drop_prob)
@@ -360,6 +360,7 @@ class QAEncoder(nn.Module):
 
     def forward(self, x):
         # Convolution layers
+        x = x.to(device)
         x = self.pos_encoder(x)         # (batch_size, seq_len, input_size)
         x = self.init_layer_norm(x)     # (batch_size, seq_len, input_size)
         x = torch.transpose(x, 1, 2)    # (batch_size, input_size, seq_len)
