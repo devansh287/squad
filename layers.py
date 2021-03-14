8464204dc9787048fdf75ff24d76c4dcce490136
Lucas-Haarmann <lh2000@stanford.edu>
2021-03-14 16:58:23 -0700

    Dellocate from GPU

--- layers.py
+++ layers.py
@@ -402,8 +402,8 @@ class QAEncoder(nn.Module):
         self.init_layer_norm = self.init_layer_norm.cpu()
         self.layer_norm = self.layer_norm.cpu()
         self.init_conv = self.init_conv.cpu()
-        for i in range(len(self.convs)):
-            self.convs[i] = self.convs[i].cpu()
+        # for i in range(len(self.convs)):
+            # self.convs[i] = self.convs[i].cpu()
         self.att = self.att.cpu()
         self.feedforward = self.feedforward.cpu()
         return x
