6938f1d6a68df397a8a98856b32a0e85b74753e5
Lucas-Haarmann <lh2000@stanford.edu>
2021-03-13 20:54:02 -0800

    f

--- layers.py
+++ layers.py
@@ -400,15 +400,15 @@ class QAEncoder(nn.Module):
         x = self.relu(x)
         x = x + start_state
 
-        """
         self.init_layer_norm = self.init_layer_norm.cpu()
         self.layer_norm = self.layer_norm.cpu()
         self.init_conv = self.init_conv.cpu()
         for i in range(len(self.convs)):
             self.convs[i] = self.convs[i].cpu()
-        self.att = self.att.cpu()
-        self.feedforward = self.feedforward.cpu()
         """
+        self.att = self.att.cpu()
+        """
+        self.feedforward = self.feedforward.cpu()
 
         return x
    
@@ -573,13 +573,11 @@ class MultiHeadSelfAttention(nn.Module):
         #del scores
         #del result
 
-        """
         #converting back
         self.key_lin=self.key_lin.cpu()
         self.query_lin=self.query_lin.cpu()
         self.val_lin=self.val_lin.cpu()
         self.out=self.out.cpu()
-        """
 
         return output
 
