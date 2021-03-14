f951808c4fbe9f7d8d63167e224e63d97ddada51
Lucas-Haarmann <lh2000@stanford.edu>
2021-03-13 20:59:11 -0800

    f

--- train.py
+++ train.py
@@ -123,13 +123,14 @@ def main(args):
                 # Forward
                 log_p1, log_p2 = model(cw_idxs, cc_idxs, qw_idxs, qc_idxs)
                 y1, y2 = y1.to(device), y2.to(device)
-                log_p1, log_p2 = log_p1.to(device), log_p2.to(device)
+                # log_p1, log_p2 = log_p1.to(device), log_p2.to(device)
                 loss = F.nll_loss(log_p1, y1) + F.nll_loss(log_p2, y2)
                 loss_val = loss.item()
 
                 # Backward
                 loss.backward()
                 nn.utils.clip_grad_norm_(model.parameters(), args.max_grad_norm)
+                print(next(model.parameters()).is_cuda)
                 optimizer.step()
                 scheduler.step(step // batch_size)
                 ema(model, step // batch_size)
