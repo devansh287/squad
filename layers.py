103d2dc46508c2f7965eb58a773bc37f9856619e
Lucas-Haarmann <lh2000@stanford.edu>
2021-03-13 20:43:11 -0800

    f

--- layers.py
+++ layers.py
@@ -363,9 +363,7 @@ class QAEncoder(nn.Module):
         #convert to cuda
         self.init_layer_norm = self.init_layer_norm.to(device)
         self.init_conv = self.init_conv.to(device)
-        """
         self.layer_norm = self.layer_norm.to(device)
-        """
         for i in range(len(self.convs)):
             self.convs[i] = self.convs[i].to(device)
         """
